1aab0856632b157efd06c25ddf6c3f861fee7577
Roman Khlebnov <suppie.rk@gmail.com>
2024-06-11 08:08:10 +0300

    Phaser skeletons

---
+++ src/main/java/io/github/suppierk/PhaserProblem.java
@@ -0,0 +1,18 @@
+package io.github.suppierk;
+
+/**
+ * No ideas what to have here as an example, leaving it as blank for now.
+ *
+ * @see <a href="https://www.baeldung.com/java-phaser">Baeldung article</a> for more details on
+ *     Phaser
+ * @see <a href="https://www.javamadesoeasy.com/2015/03/phaser-in-java_21.html">Some examples</a>
+ * @see <a
+ *     href="https://www.logicbig.com/tutorials/core-java-tutorial/java-multi-threading/phaser.html">More
+ *     examples</a>
+ * @see <a href="https://habr.com/ru/articles/277669/">Example with visualisation</a>
+ */
+public final class PhaserProblem {
+  private PhaserProblem() {
+    // No instance
+  }
+}
---
+++ src/test/java/io/github/suppierk/PhaserProblemTest.java
@@ -0,0 +1,5 @@
+package io.github.suppierk;
+
+public class PhaserProblemTest {
+  // No ideas yet
+}
